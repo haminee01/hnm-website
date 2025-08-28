c1fd21835e63e75eb08c0570b43720cfd1e4887f
haminee01 <146091591+haminee01@users.noreply.github.com>
2025-08-28 16:27:42 +0900

    distribution

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,7 @@
-import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import { thunk } from "redux-thunk";
-import rootReducer from "./reducers";
+import { configureStore } from "@reduxjs/toolkit";
 
 import productReducer from "./reducers/productReducer";
 import authenticateReducer from "./reducers/authenticateReducer";
-
-import { configureStore } from "@reduxjs/toolkit";
-
-// let store = createStore(
-//   rootReducer,
-//   composeWithDevTools(applyMiddleware(thunk))
-// );
 
 const store = configureStore({
   reducer: {
